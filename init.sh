3a13ad557b2e27d989e18897d9399bcde2edaaa2
Serghei Iakovlev <sadhooklay@gmail.com>
2015-10-18 19:14:11 +0300

    Cleanup installers. Added missed exts

--- init.sh
+++ init.sh
@@ -5,21 +5,21 @@
 # Add PHP, Phalcon, PostgreSQL and libsodium repositories
 #
 #sudo add-apt-repository -y ppa:ondrej/php5-5.6
-sudo apt-add-repository -y ppa:phalcon/stable
-sudo apt-add-repository -y ppa:chris-lea/libsodium
-sudo touch /etc/apt/sources.list.d/pgdg.list
-echo -e "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" | sudo tee -a /etc/apt/sources.list.d/pgdg.list > /dev/null
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+apt-add-repository -y ppa:phalcon/stable
+apt-add-repository -y ppa:chris-lea/libsodium
+touch /etc/apt/sources.list.d/pgdg.list
+echo -e "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" | tee -a /etc/apt/sources.list.d/pgdg.list > /dev/null
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
-sudo apt-get update
-sudo apt-get install -y python-software-properties
+apt-get update
+apt-get install -y python-software-properties
 
 #
 # Setup locales
 #
-echo -e "LC_CTYPE=en_US.UTF-8\nLC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8" | sudo tee -a /etc/environment > /dev/null
-sudo locale-gen en_US en_US.UTF-8
-sudo dpkg-reconfigure locales
+echo -e "LC_CTYPE=en_US.UTF-8\nLC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8" | tee -a /etc/environment > /dev/null
+locale-gen en_US en_US.UTF-8
+dpkg-reconfigure locales
 
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -28,7 +28,7 @@ export LC_ALL=en_US.UTF-8
 #
 # Hostname
 #
-sudo hostnamectl set-hostname phalcon-vm
+hostnamectl set-hostname phalcon-vm
 
 #
 # MySQL with root:<no password>
@@ -37,59 +37,78 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install mysql-server-5.6 mysql-client-5.6 php5-mysql
 
 #
-# PHP
-#
-sudo apt-get install -y php5 php5-cli php5-dev php-pear php5-mcrypt php5-curl php5-intl php5-xdebug php5-gd php5-imagick
-
-#
 # Apache
 #
-sudo apt-get install -y apache2 libapache2-mod-php5
+apt-get install -y apache2 libapache2-mod-php5
+
+
+#
+# PHP
+#
+apt-get install -y php5 php5-cli php5-dev php-pear php5-mcrypt php5-curl php5-intl php5-xdebug php5-gd php5-imagick
+php5enmod mcrypt intl curl
 
 #
 # Apc
 #
-sudo apt-get -y install php-apc php5-apcu
+apt-get -y install php-apc php5-apcu
+echo 'apc.enable_cli = 1' | tee -a /etc/php5/mods-available/apcu.ini > /dev/null
 
 #
 # Memcached
 #
-sudo apt-get install -y memcached php5-memcached php5-memcache
+apt-get install -y memcached php5-memcached php5-memcache
 
 #
 # MongoDB
 #
-sudo apt-get install -y mongodb-clients mongodb-server php5-mongo
+apt-get install -y mongodb-clients mongodb-server php5-mongo
 
 #
 # PostgreSQL with postgres:postgres
 # but "psql -U postgres" command don't ask password
 #
-sudo apt-get install -y postgresql-9.4
+apt-get install -y postgresql-9.4
 cp /etc/postgresql/9.4/main/pg_hba.conf /etc/postgresql/9.4/main/pg_hba.bkup.conf
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres'"
-sudo sed -i.bak -E 's/local\s+all\s+postgres\s+peer/local\t\tall\t\tpostgres\t\ttrust/g'  /etc/postgresql/9.4/main/pg_hba.conf
-sudo service postgresql restart
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres'" > /dev/null 
+sed -i.bak -E 's/local\s+all\s+postgres\s+peer/local\t\tall\t\tpostgres\t\ttrust/g' /etc/postgresql/9.4/main/pg_hba.conf
+service postgresql restart
 
 #
 # SQLite
 #
-sudo apt-get -y install sqlite php5-sqlite
+apt-get -y install sqlite php5-sqlite
 
 #
 # Beanstalkd
 #
-sudo apt-get -y install beanstalkd
+apt-get -y install beanstalkd
+
+#
+# YAML
+#
+apt-get install libyaml-dev
+printf "\n" | pecl install -a yaml
+echo 'extension=yaml.so' | tee /etc/php5/mods-available/yaml.ini > /dev/null
+php5enmod yaml
 
 #
 # Utilities
 #
-sudo apt-get install -y curl htop git dos2unix unzip vim grc gcc make re2c libpcre3 libpcre3-dev lsb-core
+apt-get install -y curl htop git dos2unix unzip vim grc gcc make re2c libpcre3 libpcre3-dev lsb-core
+
+#
+# Libsodium
+#
+apt-get install -y libsodium-dev
+printf "\n" | pecl install -a libsodium
+echo 'extension=libsodium.so' | tee /etc/php5/mods-available/libsodium.ini > /dev/null
+php5enmod libsodium
 
 #
 # Zephir
 #
-git clone https://github.com/phalcon/zephir
+git clone --depth=1 git://github.com/phalcon/zephir.git
 cd zephir
 ./install-json
 ./install -c
@@ -99,43 +118,39 @@ cd zephir
 #
 git clone --depth=1 git://github.com/phalcon/cphalcon.git
 cd cphalcon/build
-sudo ./install
-
-#
-# Libsodium
-#
-sudo apt-get install -y libsodium-dev
-sudo pecl install libsodium
+./install
+echo -e "extension=phalcon.so" | tee /etc/php5/mods-available/phalcon.ini > /dev/null
+php5enmod phalcon
 
 #
 # Redis
 #
 # Allow us to remote from Vagrant with port
 #
-sudo apt-get install -y redis-server redis-tools php5-redis
-sudo cp /etc/redis/redis.conf /etc/redis/redis.bkup.conf
-sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/' /etc/redis/redis.conf
-sudo /etc/init.d/redis-server restart
+apt-get install -y redis-server redis-tools php5-redis
+cp /etc/redis/redis.conf /etc/redis/redis.bkup.conf
+sed -i 's/bind 127.0.0.1/bind 0.0.0.0/' /etc/redis/redis.conf
+service redis-server restart
 
 #
 # MySQL configuration
 # Allow us to remote from Vagrant with port
 #
-sudo cp /etc/mysql/my.cnf /etc/mysql/my.bkup.cnf
+cp /etc/mysql/my.cnf /etc/mysql/my.bkup.cnf
 # Note: Since the MySQL bind-address has a tab cahracter I comment out the end line
-sudo sed -i 's/bind-address/bind-address = 0.0.0.0#/' /etc/mysql/my.cnf
+sed -i 's/bind-address/bind-address = 0.0.0.0#/' /etc/mysql/my.cnf
 
 #
 # Grant all priveleges to root for remote access
 #
 mysql -u root -Bse "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION;"
-sudo service mysql restart
+service mysql restart
 
 #
 # Composer for PHP
 #
-sudo curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
 
 #
 # Apache VHost
@@ -153,17 +168,9 @@ echo '<VirtualHost *:80>
         Require all granted
 </Directory>' > vagrant.conf
 
-sudo mv vagrant.conf /etc/apache2/sites-available
-sudo a2enmod rewrite
+mv vagrant.conf /etc/apache2/sites-available
+a2enmod rewrite
 
-#
-# Enable PHP5 Mods
-#
-sudo touch /etc/php5/mods-available/libsodium.ini
-sudo touch /etc/php5/mods-available/phalcon.ini
-echo -e "extension=libsodium.so" | sudo tee /etc/php5/mods-available/libsodium.ini > /dev/null
-echo -e "extension=phalcon.so" | sudo tee /etc/php5/mods-available/phalcon.ini > /dev/null
-sudo php5enmod phalcon curl mcrypt intl libsodium
 
 #
 # Install Phalcon DevTools
@@ -202,6 +209,8 @@ sudo service mongodb restart
 #  Cleanup
 #
 sudo apt-get autoremove -y
+
+sudo usermod -a -G www-data vagrant
 
 echo -e "----------------------------------------"
 echo -e "To create a Phalcon Project:\n"
